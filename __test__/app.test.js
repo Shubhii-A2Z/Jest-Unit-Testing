9cd6039c4fce5732cfe6b3d1dd4fbe26bfd25a53
unknown <negishubham2005@gmail.com>
2026-02-20 14:00:41 +0530

    feat: implement spyOn function

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -2,10 +2,14 @@ const crypto =require('crypto');
 const { getData } = require('../app');
 
 // Mocking entire import / dependency "crypto".
-jest.mock("crypto");
+// jest.mock("crypto");
 
 test('Fetch Data',async ()=>{
-    crypto.randomBytes.mockReturnValueOnce("bytes");
+    // crypto.randomBytes.mockReturnValueOnce("bytes");
+    
+    // Mocking a particular Method of a Module
+    jest.spyOn(crypto,'randomBytes').mockResolvedValueOnce('bytes');
+
     const res=await getData();
     console.log(res);
     expect(res).toBe("bytes");
